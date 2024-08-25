be478f3a2c1913a09f596f2f6bc3d0ee2dde12ea
gingerkeith <keith@redsasquatchmarketing.com>
2024-08-24 19:33:48 -0600

    missed one file

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { WagmiProvider } from "wagmi";
